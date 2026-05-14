b973fe47bf97b0efc8f01ba6884f37be495468a8
SumytRazz <sumitrajthakur@gmail.com>
2026-05-14 13:59:25 +0530

    Added automatic file moving feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 FILE_CATEGORIES = {
     ".jpg": "Images",
@@ -11,9 +12,16 @@ FILE_CATEGORIES = {
 test_folder = Path("test_files")
 
 for item in test_folder.iterdir():
+
     if item.is_file():
-        extension = item.suffix
-        category = FILE_CATEGORIES.get(extension)
+
+        if item.name == "main.py":
+            continue
+
+        extension = item.suffix.lower()
+        category = FILE_CATEGORIES.get(extension, "Others")
         folder_path = test_folder / category
         folder_path.mkdir(exist_ok=True)
-        print(f"Created folder: {folder_path}")
+        destination = folder_path / item.name
+        shutil.move(item, destination)
+        print(f"Moved: {item.name} -> {category}")
